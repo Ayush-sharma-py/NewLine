58b3d57451b8523c0e9b53b6bda185f5b330adb9
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-09-18 19:27:38 +0530

    Commit 2
    Basic Writing and Reading Functionality added

--- newline.py
+++ newline.py
@@ -17,5 +17,28 @@ def folder_read(folder_dir:str):
         content.append(file_read(dir + "//" + f))
     return content
 
+def file_write(file_dir:str,content,v = True):
+    if v:
+        print("WARNING : This function will write whatever content is in the parameter and wipe the existing data!")
+    with open(file_dir,"w+") as f:
+        for i in content:
+            for k in range(0,len(i)):
+                f.write(str(i[k]))
+                if k != len(i)-1:
+                    f.write(",")
+            f.write("\n")
+    return "File Overwritten"
 
-print(file_read("newline.txt"))
+def file_append(file_dir:str,content,v = True):
+    if v:
+        print("This Function will add data to existing function!")
+    with open(file_dir,"a+") as f:
+        for i in content:
+            for k in range(0,len(i)):
+                f.write(str(i[k]))
+                if k != len(i)-1:
+                    f.write(",")
+            f.write("\n")
+    return "File Overwritten"
+
+file_append("C:\\Users\\Ayush Sharma\\Desktop\\Programs\\newline_test.txt",[[1,2,3],[2,3],[3,4],[5,6]],True)
